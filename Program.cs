6c3371f00d1287b6cec3ab2c22853c82fb33f291
unknown <chukwuma1497@gmail.com>
2022-04-02 12:05:32 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWordapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world from wella health school");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world from wella health school");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world from wella health school");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world from wella health school");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world from wella health school");
+
+        }
+    }
+}
